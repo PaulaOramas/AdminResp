210dca1c03a1ad0ffdb3d939f93a28ee561971f8
Danahé Andrade <147558636+Danaheand@users.noreply.github.com>
2025-06-25 08:02:32 -0500

    Update +page.svelte
    se quito link ref que no son necesarioas

--- src/routes/app/usuarios/crear/+page.svelte
+++ src/routes/app/usuarios/crear/+page.svelte
@@ -90,9 +90,6 @@
 
 <svelte:head>
   <title>Agregar Usuario</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
-  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600&display=swap" rel="stylesheet" />
 </svelte:head>
 
 <div class="container mt-5">
